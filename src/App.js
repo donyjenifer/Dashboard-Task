df9cab0966d4e4695f3273dee61ee5dae988afee
donyjenifer <jenifer.dony@gmail.com>
2023-07-14 14:19:40 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import DashboardButton from './DashboardButton';
 import CreateDashboard from './CreateDashboard';
 import ChartContainer from './ChartContainer';
--- src/DashboardButton.js
+++ src/DashboardButton.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 const DashboardButton = () => {
   return (
-    <div className='dashboard'>
-   
-      <ul><Link to="/create-dashboard">Dashboard 1</Link></ul>
-      <ul><Link to="/create-dashboard"> Dashboard 2</Link></ul>
-      <ul><Link to="/create-dashboard"> Dashboard 3</Link></ul>
-   
-    </div>
+
+<Sidebar>
+  <Menu>
+    <SubMenu label="Dashboard">
+      <MenuItem>  
+      <Link to="/create-dashboard">Dashboard 1</Link></MenuItem>
+      <MenuItem> 
+       <Link to="/create-dashboard">Dashboard 2</Link></MenuItem>
+      <MenuItem> 
+       <Link to="/create-dashboard">Dashboard 3</Link></MenuItem>
+    </SubMenu>
+   </Menu>
+</Sidebar>
+
   );
 };
 
